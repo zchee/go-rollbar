e01c932f14bf01b9e1c104dd4f5dbda4b88fec07
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-17 10:36:32 +0900

    all: move {Debug,Info,Error,Warn,Critical} func to client.go

--- client.go
+++ client.go
@@ -194,3 +194,38 @@ func (c *Client) parseResponse(ctx context.Context, rdr io.Reader, data interfac
 	}
 	return json.NewDecoder(rdr).Decode(&data)
 }
+
+// Debug sends the error to rollbar with debug level.
+func (c *Client) Debug(ctx context.Context, err error) {
+	if err := c.post(ctx, DebugLevel, err); err != nil {
+		c.logger.Fatal(ctx, "rollbar: Debug: %+v", err)
+	}
+}
+
+// Info sends the error to rollbar with info level.
+func (c *Client) Info(ctx context.Context, err error) {
+	if err := c.post(ctx, InfoLevel, err); err != nil {
+		c.logger.Fatal(ctx, "rollbar: Info: %+v", err)
+	}
+}
+
+// Error sends the error to rollbar with error level.
+func (c *Client) Error(ctx context.Context, err error) {
+	if err := c.post(ctx, ErrorLevel, err); err != nil {
+		c.logger.Fatal(ctx, "rollbar: Error: %+v", err)
+	}
+}
+
+// Warn sends the error to rollbar with warn level.
+func (c *Client) Warn(ctx context.Context, err error) {
+	if err := c.post(ctx, WarnLevel, err); err != nil {
+		c.logger.Fatal(ctx, "rollbar: Warn: %+v", err)
+	}
+}
+
+// Critical sends the error to rollbar with critical level.
+func (c *Client) Critical(ctx context.Context, err error) {
+	if err := c.post(ctx, CriticalLevel, err); err != nil {
+		c.logger.Fatal(ctx, "rollbar: Critical: %+v", err)
+	}
+}
