b1f62a7dfce771b2d5c0d26a488b4547c63c646b
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-18 16:05:16 +0900

    rollbar,api, add package description

---
+++ api/v1/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The go-rollbar Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package rollbar_v1 implements Rollbar v1 REST API.
+package rollbar_v1
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The go-rollbar Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package rollbar implements Rollbar REST API client.
+package rollbar
