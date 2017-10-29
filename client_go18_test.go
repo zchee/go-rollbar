518a4aa872b35096cd1ce2afdb855ffef4cef682
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-29 22:29:51 +0900

    test/go18: fix build constants

--- client_go18_test.go
+++ client_go18_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.8,!go1.9
+// +build !go1.9
 
 // This stack trace data is Go 1.8.4.
 
